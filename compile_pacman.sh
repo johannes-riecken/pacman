2b6e03cfe709899a8043cd18c9b4d5b0ed9d0f41
Johannes Riecken <johannes.riecken@dunnhumby.com>
2019-06-06 21:44:13 +0200

    Specify package db

--- compile_pacman.sh
+++ compile_pacman.sh
@@ -1,3 +1,3 @@
 g++ -O -fPIC -o libpacman.so -shared Pacman.cpp `perl -MExtUtils::Embed -e ccopts -e ldopts`
-ghc -dynamic Pacman.hs libpacman.so 
+ghc -dynamic -package-db ~/.cabal/store/ghc-8.6.5/package.db Pacman.hs /home/jo/haskell/pacman/libpacman.so 
 LD_LIBRARY_PATH=. ./Pacman
